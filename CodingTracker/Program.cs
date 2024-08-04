4176397db87251d74d746bc10cee03874c825bbc
Supriya Gundla <Supriya.Gundla@daikinapplied.com>
2024-08-04 14:58:40 +0530

    I wrote a message

--- CodingTracker/Program.cs
+++ CodingTracker/Program.cs
@@ -14,6 +14,11 @@ namespace CodingTracker
             Console.WriteLine("Hello, C# Academy one more modification");
             Console.WriteLine("Hello, C# Academy one more modification");
             Console.WriteLine("Hello, C# Academy one more modification");
+            MyMethod();
+        }
+        static void MyMethod()
+        {
+            Console.WriteLine("Now I have a method");
         }
     }
 }
